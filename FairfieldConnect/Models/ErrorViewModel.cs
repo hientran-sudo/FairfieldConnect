1c333812097b3308dff152bb627992e0b5a6147e
hien.tran <hien.tran@NYS215-7350WLB>
2022-01-21 14:18:48 -0500

    Add project files.

---
+++ FairfieldConnect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FairfieldConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
