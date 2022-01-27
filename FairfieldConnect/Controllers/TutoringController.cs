5fff94730ab95a3f5ebafe27b18e3247152ebedd
Hien Tran <hien.tran@fairfield.edu>
2022-01-27 12:10:55 -0500

    cmt

---
+++ FairfieldConnect/Controllers/TutoringController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FairfieldConnect.Controllers
+{
+    public class TutoringController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
