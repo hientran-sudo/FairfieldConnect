4c667e22ce53931dd942222ca25861f949ab4c8c
hientran-sudo <hien.tran@student.fairfield.edu>
2022-02-03 22:24:34 -0500

    script

--- FairfieldConnect/Program.cs
+++ FairfieldConnect/Program.cs
@@ -4,11 +4,14 @@ using FairfieldConnect.Data;
 using FairfieldConnect.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("FairfieldConnectContextConnection");builder.Services.AddDbContext<FairfieldConnectContext>(options =>
-    options.UseSqlServer(connectionString));builder.Services.AddDefaultIdentity<FairfieldConnectUser>(options => options.SignIn.RequireConfirmedAccount = true)
+var connectionString = builder.Configuration.GetConnectionString("FairfieldConnectContextConnection");
+builder.Services.AddDbContext<FairfieldConnectContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDefaultIdentity<FairfieldConnectUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<FairfieldConnectContext>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
