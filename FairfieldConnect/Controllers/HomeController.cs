44c69fe096ef902e7332573727086325c63effcf
hientran-sudo <hien.tran@student.fairfield.edu>
2022-02-06 20:17:08 -0500

    cmt

--- FairfieldConnect/Controllers/HomeController.cs
+++ FairfieldConnect/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace FairfieldConnect.Controllers
 {
-    [Authorize]
+    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
