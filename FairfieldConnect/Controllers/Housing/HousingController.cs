91e606b3ded0e81c7c7ca1f17fab4e8ee2a5f680
Hien Tran <hien.tran@fairfield.edu>
2022-02-17 13:30:18 -0500

    cmt

---
+++ FairfieldConnect/Controllers/Housing/HousingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FairfieldConnect.Controllers
+{
+    public class HousingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ FairfieldConnect/Controllers/Housing/Reviews/ReviewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FairfieldConnect.Controllers.Housing.Reviews
+{
+    public class ReviewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
