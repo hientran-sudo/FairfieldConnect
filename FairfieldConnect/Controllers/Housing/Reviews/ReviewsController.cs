672d144509727bbe48baaf9f73077d934da600be
hientran-sudo <hien.tran@student.fairfield.edu>
2022-02-18 10:51:05 -0500

    cmt

--- FairfieldConnect/Controllers/Housing/Reviews/ReviewsController.cs
+++ FairfieldConnect/Controllers/Housing/Reviews/ReviewsController.cs
@@ -8,5 +8,23 @@ namespace FairfieldConnect.Controllers.Housing.Reviews
         {
             return View("~/Views/Housing/Reviews/Index.cshtml");
         }
+        public IActionResult Add()
+        {
+            return View("~/Views/Housing/Reviews/Index.cshtml");
+        }
+        //[HttpPost]
+        //public IActionResult Add(Customer c)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        context.Customers.Add(c);
+        //        context.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    else
+        //    {
+        //        return View(c);
+        //    }
+        //}
     }
 }
