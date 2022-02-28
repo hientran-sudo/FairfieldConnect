d5fab3e625fbc8f28cfa72a0431c84628116ce9e
Tom G <euphoriagtr@gmail.com>
2022-02-27 19:22:23 -0500

    Added pages for listing, adding, and deleting tutoring posts.

---
+++ FairfieldConnect/Controllers/Tutoring/TFind/TFindController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FairfieldConnect.Controllers.Tutoring.TFind
+{
+    public class TFindController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Tutoring/TFind/Index.cshtml");
+        }
+        public IActionResult Add()
+        {
+            return View("~/Views/Tutoring/TFind/Add.cshtml");
+        }
+        public IActionResult Delete()
+        {
+            return View("~/Views/Tutoring/TFind/Delete.cshtml");
+        }
+    }
+}
