3b78145e56a76cd32317f44046b50e04a021b0ba
RamKumarReddy T <ramkumar7616@gmail.com>
2022-03-16 10:39:25 -0400

    marco workspace

--- FairfieldConnect/Controllers/Market/MarketController.cs
+++ FairfieldConnect/Controllers/Market/MarketController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using Microsoft.AspNetCore.Mvc;
 
 namespace FairfieldConnect.Controllers
 {
     public class MarketController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sell()
+        {
+            return View();
+
+        }
+        public IActionResult Shop()
         {
             return View();
         }
