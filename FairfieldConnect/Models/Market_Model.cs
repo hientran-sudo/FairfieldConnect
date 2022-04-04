a76efeaad45f11b460c9a94113ffce0ee5767f7d
mpezzolla <marco.pezzolla@outlook.com>
2022-04-03 21:07:07 -0400

    marketplace models

---
+++ FairfieldConnect/Models/Market_Model.cs
@@ -0,0 +1,29 @@
+﻿/*namespace FairfieldConnect.Models
+{
+    public class Market_Model
+    {
+
+        namespace FairfieldConnect.Models
+    {
+        public class MarketPost
+        {
+            public int MarketPostID { get; set; }
+            public string Title { get; set; }
+            public string Desc { get; set; }
+            public double Price { get; set; }
+            public string Location { get; set; }
+            public date created { get; set; }
+            public date modified { get; set; }
+
+            public int StudentID { get; set; }
+            public int CategoryID { get; set; }
+
+
+            public Student Student { get; set; }
+            public Category Category { get; set; }
+
+        }
+    }
+
+}
+*/
