fea821fabd5b63ce3db7408c945cdba851dd4895
RamKumarReddy T <ramkumar7616@gmail.com>
2022-04-04 10:29:41 -0400

    Database for community

---
+++ FairfieldConnect/Models/Events_Model.cs
@@ -0,0 +1,24 @@
+﻿//namespace fairfieldconnect.models
+//{
+//    public class events_model
+//    {
+
+//        public int eventid { get; set; }
+//        public string eventtitle { get; set; }
+//        public string eventdesc { get; set; }
+//        public string eventlocation { get; set; }
+
+//        public timestamp eventhosting { get; set; }
+
+
+//        public int Studentid { get; set; }
+//        public int Clubid { get; set; }
+
+
+//        public Student Student { get; set; }
+//        public Clubs_model Clubs_model { get; set; }
+
+
+//    }
+//}
+
