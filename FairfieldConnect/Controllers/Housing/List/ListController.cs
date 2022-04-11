ef245134e6fd0f632b5c0a34b5ae68837b323000
hientran-sudo <hien.tran@student.fairfield.edu>
2022-04-11 13:01:46 -0400

    cmt

--- FairfieldConnect/Controllers/Housing/List/ListController.cs
+++ FairfieldConnect/Controllers/Housing/List/ListController.cs
@@ -1,16 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FairfieldConnect.Data;
+using FairfieldConnect.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FairfieldConnect.Controllers.Housing.List
 {
     public class ListController : Controller
     {
-        public IActionResult Index()
+        private FairfieldConnectContext context { get; set; }
+
+        public ListController(FairfieldConnectContext ctx)
         {
-            return View("~/Views/Housing/List/Index.cshtml");
+            context = ctx;
         }
+        public IActionResult Index(string id)
+        {
+           
+            IQueryable<HousingPost> query = context.HousingPosts;
+            
+
+            var hps = query.OrderBy(p => p.HousingPostID).ToList();
+
+            return View(hps);
+        }
+        [HttpGet]
         public IActionResult Add()
         {
-            return View("~/Views/Housing/List/Add.cshtml");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(HousingPost hp)
+        {
+            if (ModelState.IsValid)
+            {
+                context.HousingPosts.Add(hp);
+                context.SaveChanges();
+                return RedirectToAction("List","Index");
+            }
+            else
+            {
+               
+                return View(hp);
+            }
         }
         public IActionResult Delete()
         {
