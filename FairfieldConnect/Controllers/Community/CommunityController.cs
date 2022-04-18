8ae5a255ba45ba551a6d502ff2cf45f5ad41ae3d
hientran-sudo <hien.tran@student.fairfield.edu>
2022-04-18 14:36:03 -0400

    cmt

--- FairfieldConnect/Controllers/Community/CommunityController.cs
+++ FairfieldConnect/Controllers/Community/CommunityController.cs
@@ -143,6 +143,132 @@ namespace FairfieldConnect.Controllers
             }
         }
 
+        public IActionResult Events()
+        {
+            if (context.Events != null)
+            {
+                var e = context.Events.OrderBy(r => r.EventID).ToList();
+                return View(e);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult AddEvent()
+        {
+            if (context.Clubs != null)
+            {
+                ViewBag.Clubs = context.Clubs.ToList();
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent(Event e)
+        {
+            if (ModelState.IsValid && context.Events != null)
+            {
+                context.Events.Add(e);
+                context.SaveChanges();
+                return RedirectToAction("Events");
+            }
+            else if (context.Clubs != null)
+            {
+                ViewBag.Clubs = context.Clubs.ToList();
+                return View(e);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult DeleteEvent(int id)
+        {
+            if (context.Events != null)
+            {
+                var e = context.Events.Find(id);
+                return View(e);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public IActionResult DeleteEvent(Event e)
+        {
+
+            if (context.Events != null)
+            {
+                context.Events.Remove(e);
+                context.SaveChanges();
+                return RedirectToAction("Events");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult EventDetails(int id)
+        {
+            if (context.Events != null && context.Clubs != null)
+            {
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                Event e = context.Events.Find(id);
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+                ViewBag.Clubs = context.Clubs.ToList();
+
+                return View(e);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult EditEvent(int id)
+        {
+            if (context.Events != null && context.Clubs != null)
+            {
+                ViewBag.Clubs = context.Clubs.ToList();
+                var ent = context.Events.Find(id);
+                return View(ent);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public IActionResult EditEvent(Event ent)
+        {
+            if (ModelState.IsValid && context.Events != null)
+            {
+                context.Events.Update(ent);
+                context.SaveChanges();
+                return RedirectToAction("Events");
+            }
+            else if (context.Events != null && context.Clubs != null)
+            {
+                ViewBag.Clubs = context.Clubs.ToList();
+                return View(ent);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
 
     }
 }
